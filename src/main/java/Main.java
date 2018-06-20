f02691007cf23370cd5ba3702876fe15afd9ffd1
ione88 <Alekseev_ivan@mail.ru>
2018-06-20 14:05:01 +0300

    Signed-off-by: ione88 <Alekseev_ivan@mail.ru>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,36 @@
 import com.google.inject.*;
 import parse.*;
 
+import java.sql.SQLException;
+
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        /*Injector injector = Guice.createInjector(new YandexNewsModule());
+        Main main = new Main();
+
+        main.parseYandexNews();
+        main.parseYandexZen();
+        main.parseDnsBest();
+
+    }
+
+    private void parseYandexNews() throws SQLException {
+        Injector injector = Guice.createInjector(new YandexNewsModule());
         MyParser yandexNews = injector.getInstance(MyParser.class);
         yandexNews.makeParse();
+    }
 
+    private void parseYandexZen() throws SQLException {
+        Injector injector = Guice.createInjector(new YandexNewsModule());
+        MyParser yandexNews = injector.getInstance(MyParser.class);
+        yandexNews.makeParse();
+    }
 
-        injector = Guice.createInjector(new YandexZenModule());
-        MyParser yandexZen = injector.getInstance(MyParser.class);
-        yandexZen.makeParse();*/
-
+    private void parseDnsBest() throws SQLException {
         Injector injector = Guice.createInjector(new DnsBestModule());
         MyParser DnsBest = injector.getInstance(MyParser.class);
         DnsBest.makeParse();
-
     }
 }
